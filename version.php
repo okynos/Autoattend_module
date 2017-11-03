78fb2b5b9adf13939208883848fda08d4ff7fcc9
José Fernández Aguilera <pylott@gmail.com>
2017-11-03 14:00:29 +0100

    Initial commit
    Changes in 2.5.2 - Removed generation of Csv and plain text data to upgrade efficiency in large courses.
    Changes in 2.5.3 - Added Some Timezone control because the plugin not adjust time well with daylight save time.

---
+++ version.php
@@ -0,0 +1,12 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die();
+
+
+$plugin->requires  = 2011120500;			// Moodle 2.2
+$plugin->component = 'block_autoattend';	// Full name of the plugin (used for diagnostics)
+$plugin->cron      = 0;
+$plugin->maturity  = MATURITY_STABLE;
+
+$plugin->version   = 2017110300;			// The current plugin version (Date: YYYYMMDDXX)
+$plugin->release   = '2.5.3';
